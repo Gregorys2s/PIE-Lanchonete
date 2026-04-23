dcdbfab4bd955245dc86afd19c8da2f1185f1132
Gregory <necrondragonss@gmail.com>
2026-04-22 21:24:00 -0300

    Foi definido os seguintes pasos e criada a classe Caixa

--- Lanchonete/src/main/java/com/github/Gregorys2s/Main.java
+++ Lanchonete/src/main/java/com/github/Gregorys2s/Main.java
@@ -3,6 +3,12 @@ package com.github.Gregorys2s;
 
 public class Main {
     public static void main(String[] args) {
-
+        //abre a caixa apenas o codigo roda, pede o valor da caixa inicial, se for cancelado ele faz um break e finaliza o codigo
+        //aqui sera inicializado no o programa e a conexao com o banco de dados
+        //ela sera uma classe conection e sera fechada no final do codigo
+        //aqui e inizializado a classe que contem o menu
+        //ele pede a opcao escolhida e processa para os proximos passo.
+        //Estrutura de modelagem para o projecto
+        //
     }
 }
