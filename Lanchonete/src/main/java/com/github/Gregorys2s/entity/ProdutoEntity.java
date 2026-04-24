8cc3a4fac9b297c131f8c88c3883f08f44fe5907
Guilherme <guilherme.xande25@gmail.com>
2026-04-23 21:29:09 -0300

    proceso de configurar os drivers ainda

---
+++ Lanchonete/src/main/java/com/github/Gregorys2s/entity/ProdutoEntity.java
@@ -0,0 +1,47 @@
+package com.github.Gregorys2s.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Collum(nullable = false, lenght = 100)
+    private String nome;
+
+    @Collum(nullable = false, precision = 10, scale = 2)
+    private java.math.BigDecimal preco;
+
+    public Produto() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
