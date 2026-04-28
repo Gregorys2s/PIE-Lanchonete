edfe172f8e4985b7937aaef9592d4116cbc3e468
Guilherme <guilherme.xande25@gmail.com>
2026-04-28 20:35:21 -0300

    Terminado a criação de exceptions

---
+++ Lanchonete/src/main/java/com/github/Gregorys2s/exceptions/EstoqueInvalidoException.java
@@ -0,0 +1,7 @@
+package com.github.Gregorys2s.exceptions;
+
+public class EstoqueInvalidoException extends RuntimeException {
+    public EstoqueInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
