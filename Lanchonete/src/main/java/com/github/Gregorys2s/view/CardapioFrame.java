7eee5534cde53f4cbaf839a9698f958eea7c4461
pedro <pedro.m.dias.d.moraes@gmail.com>
2026-05-09 00:02:21 -0300

    Pesquisa + filtro feita indo para Crud

--- Lanchonete/src/main/java/com/github/Gregorys2s/view/CardapioFrame.java
+++ Lanchonete/src/main/java/com/github/Gregorys2s/view/CardapioFrame.java
@@ -5,15 +5,14 @@
 package com.github.Gregorys2s.view;
 
 import com.github.Gregorys2s.controller.CardapioController;
+import javax.swing.RowFilter;
+import javax.swing.table.TableRowSorter;
 
-/**
- *
- * @author Aluno 116
- */
 public class CardapioFrame extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(CardapioFrame.class.getName());
     private final CardapioController cardapioController;
+    private TableRowSorter<CardapioView> sorter;
     private CardapioView cardapioView;
     /**
      * Creates new form CardapioFrame
@@ -29,8 +28,10 @@ public class CardapioFrame extends javax.swing.JFrame {
     private void CardapioConfig()
     {
         this.cardapioView = new CardapioView(cardapioController.obterLista());
-
+        sorter = new TableRowSorter<>(cardapioView);
         jTable1.setModel(cardapioView);
+        jTable1.setRowSorter(sorter);
+
     }
     /**
      * This method is called from within the constructor to initialize the form.
@@ -42,13 +43,29 @@ public class CardapioFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        comboFiltroSelector = new javax.swing.JComboBox<>();
+        jScrollPane2 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
+        stringRecebida = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 519, Short.MAX_VALUE)
+        );
+
+        comboFiltroSelector.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ID", "Nome", "Tipo", " " }));
+        comboFiltroSelector.setSelectedIndex(1);
+        comboFiltroSelector.addActionListener(this::comboFiltroSelectorActionPerformed);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -67,46 +84,94 @@ public class CardapioFrame extends javax.swing.JFrame {
             }
         });
         jTable1.setColumnSelectionAllowed(true);
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane2.setViewportView(jTable1);
         jTable1.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 609, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 12, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
+        stringRecebida.setText("jTextField1");
+        stringRecebida.addActionListener(this::stringRecebidaActionPerformed);
+        stringRecebida.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                stringRecebidaKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                stringRecebidaKeyTyped(evt);
+            }
+        });
 
-        jMenu1.setText("Items");
-        jMenuBar1.add(jMenu1);
+        jLabel1.setText("Pesquisa:");
 
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
-
-        setJMenuBar(jMenuBar1);
+        jLabel2.setText("Filtro:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(stringRecebida, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(comboFiltroSelector, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(19, 19, 19)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(comboFiltroSelector, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(stringRecebida, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 403, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void comboFiltroSelectorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboFiltroSelectorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_comboFiltroSelectorActionPerformed
+
+    private void stringRecebidaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_stringRecebidaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_stringRecebidaActionPerformed
+
+    private void stringRecebidaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_stringRecebidaKeyTyped
+ 
+    }//GEN-LAST:event_stringRecebidaKeyTyped
+
+    private void stringRecebidaKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_stringRecebidaKeyReleased
+           String receptor = stringRecebida.getText().trim();
+            String filtro = comboFiltroSelector.getSelectedItem().toString();
+
+            if(receptor.isBlank() || receptor.isEmpty())
+            {
+                sorter.setRowFilter(null);
+            }
+            switch (filtro) {
+            case "Nome" -> {
+                sorter.setRowFilter(RowFilter.regexFilter("(?i)" + receptor, 1));
+            }
+            case "Tipo" -> {
+                sorter.setRowFilter(RowFilter.regexFilter("(?i)" + receptor, 2));
+            }
+            case "ID" -> {
+                sorter.setRowFilter(null);
+            }
+        }
+    }//GEN-LAST:event_stringRecebidaKeyReleased
 
     /**
      * @param args the command line arguments
@@ -134,11 +199,13 @@ public class CardapioFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JComboBox<String> comboFiltroSelector;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTextField stringRecebida;
     // End of variables declaration//GEN-END:variables
 }
