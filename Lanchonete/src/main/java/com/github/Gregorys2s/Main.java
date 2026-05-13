a97f8c878a0a6e56a8ddb618a38aa43545b37ff7
FelipeYuji <fjapa977@gmail.com>
2026-05-13 00:52:28 -0300

    Interface do estoque de ingredientes feita, com todos os requisitos pedidos

--- Lanchonete/src/main/java/com/github/Gregorys2s/Main.java
+++ Lanchonete/src/main/java/com/github/Gregorys2s/Main.java
@@ -1,14 +1,27 @@
 package com.github.Gregorys2s;
 
 import com.github.Gregorys2s.config.AppConfig;
+import com.github.Gregorys2s.controller.IngredientesController; // import adicinado (Yuji)
+import com.github.Gregorys2s.service.IngredientesService; // import adicinado (Yuji)
+import com.github.Gregorys2s.view.IngredientePanel; // import adicinado (Yuji)
 import com.github.Gregorys2s.view.Inicializar;
+import javax.swing.SwingUtilities;
 
 public class    Main {
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
 
         Inicializar init = AppConfig.configSistema();
         init.inicializarSistema();
 
-    }
+        IngredientesService service = new IngredientesService();
+        IngredientesController controller = new IngredientesController(service);
 
+        SwingUtilities.invokeLater(() -> {
+            IngredientePanel tela = new IngredientePanel(controller);
+
+            // Garante que a tela só fique visível APÓS tudo ser construído internamente
+            tela.setVisible(true);
+        });
+    }
 }
